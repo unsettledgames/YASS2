a5c680c8f656e1ce4c4c8ba71a394a4b4ce42fa6
unsettledgames <47360416+unsettledgames@users.noreply.github.com>
2021-04-16 12:55:45 +0200

    Tried an asteroid field

---
+++ Assets/Scripts/AmbianceManagement/AsteroidField.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidField : MonoBehaviour
+{
+    public GameObject[] toInstantiate;
+    public float instantiateDistance;
+    public float fieldRadius;
+    public float nAsteroids;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(InstantiateAsteroids());
+    }
+
+    private IEnumerator InstantiateAsteroids()
+    {
+        Vector3 pos = new Vector3();
+        Vector3 rot = new Vector3();
+
+        for (int i=0; i<nAsteroids; i++)
+        {
+            pos.x = Random.Range(-fieldRadius, fieldRadius);
+            pos.y = Random.Range(-fieldRadius, fieldRadius);
+            pos.z = Random.Range(-fieldRadius, fieldRadius);
+
+            rot.x = Random.Range(0, 360);
+            rot.y = Random.Range(0, 360);
+            rot.z = Random.Range(0, 360);
+
+            Instantiate(toInstantiate[Random.Range(0, toInstantiate.Length)], pos, Quaternion.Euler(rot));
+
+            Debug.Log(i);
+
+            yield return null;
+        }
+
+        Debug.Log("finished");
+
+        yield return null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Enemies/AsteroidAI.cs
+++ Assets/Scripts/Enemies/AsteroidAI.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class AsteroidAI : SpawnedEnemy
 {
+    public float colliderActivationDistance = 30f;
     public float maxRotationMagnitude;
     public float maxVelocityMagnitude;
+
+    private Collider collider;
+    private GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
         Rigidbody physics = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
+        player = FrequentlyAccessed.Instance.player;
 
 
         physics.velocity = new Vector3(
@@ -22,5 +28,13 @@ public class AsteroidAI : SpawnedEnemy
             Random.Range(-1f, 1f),
             Random.Range(-1f, 1f),
             Random.Range(-1f, 1f)).normalized * maxRotationMagnitude;
+
+        collider.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(player.transform.position, transform.position) < colliderActivationDistance)
+            collider.enabled = true;
     }
 }
